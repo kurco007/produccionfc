2a70176fa3f60ec2f6922d549e03ed88284305ba
Kurt Cruz Garcia <kurtc.garcia@gmail.com>
2022-11-17 16:47:49 -0700

    s

--- page-formato1.php
+++ page-formato1.php
@@ -28,96 +28,82 @@
                                 <p> <?php the_field('piloncillo'); ?>y</p>
                                 <p> <?php the_field('nuez'); ?> y</p>
                             </div>
-                            <div class="sabores">
-                                <div class="cantidad">
-                                    <p> <?php the_field('charolas_cajeta_coyotas'); ?>charolas</p>
-                                    <p> <?php the_field('charolas_jamoncillo_coyotas'); ?>charolas</p>
-                                    <p> <?php the_field('charolas_piloncillo_coyotas'); ?>charolas</p>
-                                    <p> <?php the_field('charolas_nuez_coyotas'); ?>charolas</p>
-                                </div>
-                                <div class="sabor">
-                                    <p>Cajeta</p>
-                                    <p>Jamoncillo</p>
-                                    <p>Piloncillo</p>
-                                    <p>Nuez</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mini-coyotas">
-                        <h2>Mini Coyotas</h2>
-                        <table style="width: 100%;">
-                            <tr>
-                                <th>Espiqueros Totales</th>
-                                <th>Espigueros por Sabor</th>
-                            </tr>
-                        </table>
-                        <div class="contenido">
-                            <div class="estantes">
-                                <h1> <?php the_field('espigueros_totales_mini'); ?></h1>
-                            </div>
-                            <div class="sabores">
-                                <div class="cantidad">
-                                    <p> <?php the_field('cajeta_mini'); ?></p>
-                                    <p> <?php the_field('lechera_mini'); ?></p>
-                                </div>
-                                <div class="cantidad">
-                                    <p> <?php the_field('charolas_cajeta_mini'); ?>charolas</p>
-                                    <p> <?php the_field('charolas_lechera_mini'); ?>charolas</p>
-                                </div>
-                                <div class="sabor">
-                                    <p>Cajeta</p>
-                                    <p>Lechera</p>
-                                </div>
+
+                            <div class="sabor">
+                                <p>Cajeta</p>
+                                <p>Jamoncillo</p>
+                                <p>Piloncillo</p>
+                                <p>Nuez</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="seccion2">
-                    <div class="empanadas">
-                        <h2>Empanadas</h2>
-                        <table style="width: 100%;">
-                            <tr>
-                                <th>Espigueros Totales</th>
-                                <th>Espiguero por Sabor</th>
-                            </tr>
-                        </table>
-                        <div class="contenido">
-                            <div class="estantes">
-                                <h1> <?php the_field('espigueros_totales_empanadas'); ?></h1>
+                <div class="mini-coyotas">
+                    <h2>Mini Coyotas</h2>
+                    <table style="width: 100%;">
+                        <tr>
+                            <th>Espiqueros Totales</th>
+                            <th>Espigueros por Sabor</th>
+                        </tr>
+                    </table>
+                    <div class="contenido">
+                        <div class="estantes">
+                            <h1> <?php the_field('espigueros_totales_mini'); ?></h1>
+                        </div>
+                        <div class="sabores">
+                            <div class="cantidad">
+                                <p> <?php the_field('cajeta_mini'); ?></p>
+                                <p> <?php the_field('lechera_mini'); ?></p>
                             </div>
-                            <div class="sabores">
-                                <div class="cantidad">
-                                    <p> <?php the_field('cajeta_empanadas');  ?> y</p>
-                                    <p> <?php the_field('lechera_empanadas'); ?>y</p>
-                                </div>
-                                <div class="cantidad">
-                                    <p> <?php the_field('charolas_cajeta_empanads'); ?>charolas</p>
-                                    <p> <?php the_field('charolas_lechera_empanads'); ?>charolas</p>
-                                </div>
-                                <div class="sabor">
-                                    <p>Cajeta</p>
-                                    <p>Lechera</p>
-                                </div>
+                            <div class="sabor">
+                                <p>Cajeta</p>
+                                <p>Lechera</p>
                             </div>
                         </div>
                     </div>
-                    <div class="coricos">
-                        <h2>Coricos</h2>
-                        <table style="width: 100%;">
-                            <tr>
-                                <th>Espigueros Totales</th>
-                                <th>Charolas totales</th>
-                            </tr>
-                        </table>
-                        <div class="contenido">
-                            <div class="estantes">
-                                <h1> <?php the_field('espigueros_totales_coricos'); ?></h1>
+                </div>
+            </div>
+            <div class="seccion2">
+                <div class="empanadas">
+                    <h2>Empanadas</h2>
+                    <table style="width: 100%;">
+                        <tr>
+                            <th>Espigueros Totales</th>
+                            <th>Espiguero por Sabor</th>
+                        </tr>
+                    </table>
+                    <div class="contenido">
+                        <div class="estantes">
+                            <h1> <?php the_field('espigueros_totales_empanadas'); ?></h1>
+                        </div>
+                        <div class="sabores">
+                            <div class="cantidad">
+                                <p> <?php the_field('cajeta_empanadas');  ?> y</p>
+                                <p> <?php the_field('lechera_empanadas'); ?>y</p>
                             </div>
-                            <div class="sabores contenido">
-                                <div class="estantes">
-                                    <h1> <?php the_field('total_de_charolas'); ?></h1>
-                                </div>
+
+                            <div class="sabor">
+                                <p>Cajeta</p>
+                                <p>Lechera</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="coricos">
+                    <h2>Coricos</h2>
+                    <table style="width: 100%;">
+                        <tr>
+                            <th>Espigueros Totales</th>
+                            <th>Charolas totales</th>
+                        </tr>
+                    </table>
+                    <div class="contenido">
+                        <div class="estantes">
+                            <h1> <?php the_field('espigueros_totales_coricos'); ?></h1>
+                        </div>
+                        <div class="sabores contenido">
+                            <div class="estantes">
+                                <h1> <?php the_field('total_de_charolas'); ?></h1>
                             </div>
                         </div>
                     </div>
@@ -125,5 +111,6 @@
             </div>
         </div>
     </div>
-    <div class="slider">
-    </div>
+</div>
+<div class="slider">
+</div>
